20593634ec9839ec7419860701f24c72c9b47e81
kenwschen <ken00535@gmail.com>
2020-09-26 02:10:45 +0800

    test(dao): add tests

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module dao
 
 go 1.14
 
-require github.com/ken00535/dao v0.0.0-20200925150721-ca972cd91e2a
+require (
+	github.com/ken00535/dao v0.0.0-20200925150721-ca972cd91e2a
+	github.com/stretchr/testify v1.6.1
+)
 
 replace github.com/ken00535/dao => ../dao
---
+++ pkg/filter_test.go
@@ -0,0 +1,58 @@
+package dao
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Person struct {
+	Name   string
+	Gender string
+}
+
+func TestFilter(t *testing.T) {
+	people := []Person{{Name: "Ken", Gender: "Male"}, {Name: "Cythia", Gender: "Female"}}
+	actual := []Person{}
+	expect := []Person{{Name: "Ken", Gender: "Male"}}
+	Filter(people, &actual, func(person interface{}) bool {
+		return person.(Person).Name == "Ken"
+	})
+	assert.Equal(t, expect, actual)
+}
+
+func TestReduce(t *testing.T) {
+	people := []Person{{Name: "Ken", Gender: "Male"}, {Name: "Cythia", Gender: "Female"}}
+	expect := Person{Name: "Ken Cythia"}
+	actual := Person{}
+	Reduce(people, &actual, func(m interface{}, n interface{}) interface{} {
+		person := Person{
+			Name: m.(Person).Name + " " + n.(Person).Name,
+		}
+		return person
+	})
+	assert.Equal(t, expect, actual)
+}
+
+func TestMap(t *testing.T) {
+	people := []Person{{Name: "Ken", Gender: "Male"}, {Name: "Cythia", Gender: "Female"}}
+	expect := []Person{{Name: "ken", Gender: "Male"}, {Name: "cythia", Gender: "Female"}}
+	actual := []Person{}
+	Map(people, &actual, func(person interface{}) interface{} {
+		val := person.(Person)
+		val.Name = strings.ToLower(val.Name)
+		return val
+	})
+	assert.Equal(t, expect, actual)
+}
+
+func TestFind(t *testing.T) {
+	people := []Person{{Name: "Ken", Gender: "Male"}, {Name: "Cythia", Gender: "Female"}}
+	expect := Person{Name: "Cythia", Gender: "Female"}
+	actual := Person{}
+	Find(people, &actual, func(person interface{}) bool {
+		return person.(Person).Name == "Cythia"
+	})
+	assert.Equal(t, expect, actual)
+}
