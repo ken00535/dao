69f38cd2f615f36743f9cec0bcaf67228c903e45
kenwschen <ken00535@gmail.com>
2020-09-26 00:28:19 +0800

    feat(dao): add Find, ForEach, Map, Reduce, Omit

--- cmd/main.go
+++ cmd/main.go
@@ -15,13 +15,31 @@ func main() {
 	num2 := []People{{Name: "Ken", Address: "TW"}, {Name: "Cythia"}}
 	new := []People{}
 	dao.Filter(num2, &new, func(element interface{}) bool {
-		if element.(People).Name == "Ken" {
-			return true
-		}
-		return false
+		return element.(People).Name == "Ken"
 	})
-	fmt.Println(new)
 	for i := 0; i < len(new); i++ {
 		fmt.Println(new[i])
 	}
+
+	new2 := People{}
+	dao.Find(num2, &new2, func(element interface{}) bool {
+		return element.(People).Name == "Cythia"
+	})
+	fmt.Println(new2)
+
+	new3 := []People{}
+	dao.Map(num2, &new3, func(element interface{}) interface{} {
+		val := element.(People)
+		val.Name = val.Name + "_EX"
+		return val
+	})
+	fmt.Println(new3)
+
+	new4 := People{}
+	dao.Reduce(num2, &new4, func(m interface{}, n interface{}) interface{} {
+		people := People{}
+		people.Name = m.(People).Name + n.(People).Name
+		return people
+	})
+	fmt.Println(new4)
 }
--- pkg/filter.go
+++ pkg/filter.go
@@ -4,11 +4,69 @@ import (
 	"reflect"
 )
 
+// Find the first element that correspond condition
+func Find(source interface{}, out interface{}, callback func(element interface{}) bool) {
+	sourceValue := reflect.ValueOf(source)
+	temp := reflect.ValueOf(out).Elem()
+	for i := 0; i < sourceValue.Len(); i++ {
+		if callback(sourceValue.Index(i).Interface()) {
+			temp.Set(sourceValue.Index(i))
+			break
+		}
+	}
+}
+
+// Filter return the elements that correspond condition
 func Filter(source interface{}, out interface{}, callback func(element interface{}) bool) {
 	sourceValue := reflect.ValueOf(source)
 	temp := reflect.ValueOf(out).Elem()
 	for i := 0; i < sourceValue.Len(); i++ {
 		if callback(sourceValue.Index(i).Interface()) {
+			temp = reflect.Append(temp, sourceValue.Index(i))
+		}
+	}
+	outValue := reflect.ValueOf(out).Elem()
+	outValue.Set(temp)
+}
+
+// ForEach visit each element and call callback
+func ForEach(source interface{}, out interface{}, callback func(element interface{})) {
+	sourceValue := reflect.ValueOf(source)
+	for i := 0; i < sourceValue.Len(); i++ {
+		callback(sourceValue.Index(i).Interface())
+	}
+}
+
+// Map element to a new slice
+func Map(source interface{}, out interface{}, callback func(element interface{}) interface{}) {
+	sourceValue := reflect.ValueOf(source)
+	temp := reflect.ValueOf(out).Elem()
+	for i := 0; i < sourceValue.Len(); i++ {
+		mapVal := callback(sourceValue.Index(i).Interface())
+		temp = reflect.Append(temp, reflect.ValueOf(mapVal))
+	}
+	outValue := reflect.ValueOf(out).Elem()
+	outValue.Set(temp)
+}
+
+// Reduce slice to a value
+func Reduce(source interface{}, out interface{}, callback func(interface{}, interface{}) interface{}) {
+	sourceValue := reflect.ValueOf(source)
+	reduce := callback(sourceValue.Index(0).Interface(), sourceValue.Index(1).Interface())
+	for i := 2; i < sourceValue.Len(); i++ {
+		reduce = callback(reduce, sourceValue.Index(i).Interface())
+	}
+	reduceValue := reflect.ValueOf(reduce)
+	outValue := reflect.ValueOf(out).Elem()
+	outValue.Set(reduceValue)
+}
+
+// Omit delete the elements that correspond condition
+func Omit(source interface{}, out interface{}, callback func(element interface{}) bool) {
+	sourceValue := reflect.ValueOf(source)
+	temp := reflect.ValueOf(out).Elem()
+	for i := 0; i < sourceValue.Len(); i++ {
+		if !callback(sourceValue.Index(i).Interface()) {
 			temp = reflect.Append(temp, sourceValue.Index(i))
 		}
 	}
